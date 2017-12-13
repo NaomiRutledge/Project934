39ea3ad55ecd1b6fcbe3c7a2bf404570655e7a16
Naomi Rutledge <32394816+NaomiRutledge@users.noreply.github.com>
2017-12-13 16:48:33 +0000

    Changed Demand Set h
    Setting all variables to be a function of 'h'. This is due to an issue brought up today by Euan and Alex where the demand script may not match the data. By being able to run the Demand script for any value of 'h' (chosen by the user), then this issue should be minimised.

--- Demand_12_12_2.m
+++ Demand_12_12_2.m
@@ -1,6 +1,6 @@
 %Data Sets
     %HourPeriod
-    h=6;
+    h=4;
     t1=datetime(2010,1,1,0,0,0);
     t2=datetime(2010,12,31,23,0,0);
     jan=datetime(2010,1,1,0,0,0);
@@ -26,7 +26,7 @@
     T_ambmin=[3.8 3.2 4 5.05 6.8 9.5 11 11 10 8 5.5 4];
     T_ambmax=[7.6 7.5 8.2 10.5 12.8 14.9 16 16 15 12.5 10 8];
     T_amb=(T_ambmax+T_ambmin)/2;
-    T_amb2=repelem(T_amb, [length(jan:hours(h):feb)-1 length(feb:hours(h):mar)-1 length(mar:hours(h):apr)-1 length(apr:hours(h):may)-1 length(may:hours(h):jun)-1 length(jun:hours(h):jul)-1 length(jul:hours(h):aug)-1 length(aug:hours(h):sep)-1 length(sep:hours(h):oct)-1 length(oct:hours(h):nov)-1 length(nov:hours(h):dec)-1 length(dec:hours(h):t2)]);
+    T_amb2=repelem(T_amb, [(length(jan:1:feb)-1)*24/h (length(feb:1:mar)-1)*24/h (length(mar:1:apr)-1)*24/h (length(apr:1:may)-1)*24/h (length(may:1:jun)-1)*24/h (length(jun:1:jul)-1)*24/h (length(jul:1:aug)-1)*24/h (length(aug:1:sep)-1)*24/h (length(sep:1:oct)-1)*24/h (length(oct:1:nov)-1)*24/h (length(nov:1:dec)-1)*24/h length(dec:1:t2)*24/h]);
     
 run Milk_12_12_2
 Q_totalyearmc;
