293865f38e8993a390f81c8af53df4bb113a405f
NaomiRutledge <naomi.rutledge.2013@uni.strath.ac.uk>
2017-12-17 21:34:13 +0000

    Minor Changes

--- Demand_12_12_2.m
+++ Demand_12_12_2.m
@@ -9,6 +9,7 @@ run Water_Heating_12_12_2; Q_totalyearwh;
 run Milking_Machine_12_12_1; Q_totalyearmm;
 run Lighting_13_12_1; Q_totalyearl;
 
+
 Q_all_matrix=[Q_totalyearmc Q_totalyearwh Q_totalyearmm Q_totalyearl];
 Q_total=sum(Q_all_matrix);
 Q_all=Q_mc+Q_wh+Q_mm+Q_l;
