304e2d0672b731aeb31dd96123e22c0fabe0b004
NaomiRutledge <naomi.rutledge.2013@uni.strath.ac.uk>
2017-12-17 13:46:44 +0000

    Added Links

--- Milking_Machine_12_12_1.m
+++ Milking_Machine_12_12_1.m
@@ -1,14 +1,15 @@
 cows;
 %http://www.agriland.ie/farming-news/how-many-cows-can-one-person-milk-its-not-as-straightforward-as-you-may-think/
-cluster=20;
+%https://www.aliexpress.com/store/product/For-Home-Use-Diary-Farm-Use-Stainless-Steel-4-Buckets-Vacuum-Pump-Portable-Milking-Machines-for/1922474_32465142342.html
+cluster=40;
 Ratio1=cows/cluster;
-t_row=TimeRatio*12/60; %Assumes that Milk requires cooled ASAP after milking.
+%Using SPS-MM03
+t_row=TimeRatio/h; %Assumes that Milk requires cooled ASAP after milking.
 t_row=repelem(t_row,1,365);
 t_row=reshape(t_row,1,(365*(24/h)));
-
 %Washing machinery for 10 mins, every hour for 8 hours?
 t_wash=MilkTime1;
 %http://www.solpack.in/products/calalogue/cow%20milking%20machine.pdf
-C_mm=1.1;
+C_mm=2.2;
 Q_mm=((ceil(Ratio1).*t_row)+t_wash)*C_mm;
-Q_totalyearmm=sum(Q_mm);
+Q_totalyearmm=sum(Q_mm)
