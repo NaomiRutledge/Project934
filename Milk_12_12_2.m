36b911c5e08f85847f7239af9b286e8ba7f677d5
NaomiRutledge <naomi.rutledge.2013@uni.strath.ac.uk>
2017-12-17 16:18:46 +0000

    Reverting Back to Original Method

--- Milk_12_12_2.m
+++ Milk_12_12_2.m
@@ -9,7 +9,25 @@
     DailyMilk=(cows*4662.39*ro_milk)/(days*0.688818719)*(-2*(10^-10).*x.^4+2*(10^-07).*x.^3-8*(10^-05).*x.^2+0.0116.*x+0.4352);
     %Ratio of milk produced every hour to max milk production. Should be +-
     %a certain amount. Based on https://strath-my.sharepoint.com/:x:/r/personal/npb13178_uni_strath_ac_uk/_layouts/15/WopiFrame.aspx?sourcedoc=%7BAFD5B4F7-09BD-44AE-8019-6E9053BE82F9%7D&file=Milk%20Produced%20Per%20Cow,%20States%20and%20United%20States%20(1).csv&action=default
-   
+     
+    if h==1
+        TimeRatio=[0;0;0;0;0;0.1;0.1;0.2;0.2;0.2;0.1;0.05;0.05;0;0;0;0;0;0;0;0;0;0;0];
+    elseif h==2
+            TimeRatio=[0;0;0.1;0.3;0.4;0.15;0.05;0;0;0;0;0];
+    elseif h==3
+                TimeRatio=[0;0.1;0.5;0.35;0.05;0;0;0]; 
+    elseif h==4
+                        TimeRatio=[0;0.5;0.45;0.05;0;0];
+    elseif h==6
+                            TimeRatio=[0.1;0.85;0.05;0];
+    elseif h==8
+                                TimeRatio=[0.5;0.5;0];
+    elseif h==12
+                                    TimeRatio=[0.95;0.05];
+    elseif h==24
+                                        TimeRatio=1;
+    end
+    
     HourlyMilkProduction=TimeRatio*DailyMilk;
     HourlyMilk=reshape(HourlyMilkProduction,1,(365*24/h));
     TotalYearlyMilk=sum(DailyMilk);
@@ -25,7 +43,8 @@
     %Capacity of the MilkCoolers
     %http://www.fao.org/3/a-i5791e.pdf - chosen the least efficient one
     
-    %Milktime is 
+    %Milktime is the time taken to cool the milk. Not required in this case
+    %since this time is an estimated input. 
     MilkTime1=Q_mc./C_mc;
     
     plot(ts1,MilkTime1);
